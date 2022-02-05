f7ac5c7b2a4b683e370c294f6e49676fe5ed4619
Dmitry <70606894+g0rg0l@users.noreply.github.com>
2022-02-05 16:26:54 +0300

    Update comparingOfSorts.h

--- comparingOfSorts/comparingOfSorts.h
+++ comparingOfSorts/comparingOfSorts.h
@@ -1,6 +1,3 @@
-#ifndef BOOK_ABOUT_CPP_COMPARINGOFSORTS_H
-#define BOOK_ABOUT_CPP_COMPARINGOFSORTS_H
-
 void initMass(int *array, int n);
 
 void showMass(int *array, int n);
@@ -8,5 +5,3 @@ void showMass(int *array, int n);
 int sortByChoice(int *array, int n);
 
 int sortByShaker(int *array, int n);
-
-#endif //BOOK_ABOUT_CPP_COMPARINGOFSORTS_H
