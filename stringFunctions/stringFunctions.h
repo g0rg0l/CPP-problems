99935e3ca40a6296473e53d527bf2733a79008b5
Dmitry <70606894+g0rg0l@users.noreply.github.com>
2022-02-05 16:29:49 +0300

    Update stringFunctions.h

--- stringFunctions/stringFunctions.h
+++ stringFunctions/stringFunctions.h
@@ -1,6 +1,3 @@
-#ifndef BOOK_ABOUT_CPP_STRINGFUNCTIONS_H
-#define BOOK_ABOUT_CPP_STRINGFUNCTIONS_H
-
 int getLength(const char *str);
 
 char *reverse(char *str);
@@ -12,5 +9,3 @@ int searchForSubstring(char *str, char *substr);
 char *insertSubstring (char *str, char *substr, int index);
 
 char *getSubstring(char *str, int k, int n);
-
-#endif //BOOK_ABOUT_CPP_STRINGFUNCTIONS_H
